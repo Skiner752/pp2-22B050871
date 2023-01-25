1ca0420271dbf42ae090d2ed6fa47339cbe9dcd9
Ruslan <ruslanpuslan@gmail.com>
2023-01-25 14:49:21 +0600

    Second Commit

---
+++ tsis2/arrays.py
@@ -0,0 +1,14 @@
+# Arrays 
+ cars = ["Ford", "Volvo", "BMW"]
+ x = cars[0]
+ cars[0] = "Toyota"
+# The lenght of an array 
+ x = len(cars)
+# Looping Array Elements
+ for x in cars:
+    print(x)
+# Adding Array Elements
+ cars.append("Honda")
+# Removing Array Elements
+ cars.pop(1)
+ cars.remove("Volvo")
